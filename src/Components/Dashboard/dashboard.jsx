56f8a63f213f18d2827c5d34c59a6c46e33300cc
Álvaro de oliveira leal <97075018+ALVAROLEAL77@users.noreply.github.com>
2024-01-17 02:25:08 -0300

    Update dashboard.jsx

--- src/Components/Dashboard/dashboard.jsx
+++ src/Components/Dashboard/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import SideBar from '../Dashboard/Components/SideBar Section/sidebar'
-import Body from '../Dashboard/Components/Body Section/body'
+import SideBar from '../Dashboard/Components/SideBar Section/Sidebar'
+import Body from '../Dashboard/Components/Body Section/Body'
 
 const Dashboard = () => {
     return(
